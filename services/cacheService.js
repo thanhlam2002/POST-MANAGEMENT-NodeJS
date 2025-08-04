55e313b2c5d644722ea359c79cef6e04c0e23141
nguyenlam <lthanhnguyen951@gmail.com>
2025-08-04 19:50:10 +0700

    Update cacheService.js

--- services/cacheService.js
+++ services/cacheService.js
@@ -1,22 +1,23 @@
-// const redis = require('redis');
+const redis = require('redis');
 
-// // Tạo Redis client và kết nối
-// const client = redis.createClient();
-// client.connect().catch(console.error);
+// Tạo Redis client và kết nối
+const client = redis.createClient();
+client.connect().catch(console.error);
 
-// // Hàm lấy dữ liệu từ Redis Cache
-// exports.getCache = async (key) => {
-//     const data = await client.get(key);
-//     return data ? JSON.parse(data) : null;  // Nếu có cache thì trả về dữ liệu JSON
-// };
+// Hàm lấy dữ liệu từ Redis Cache
+exports.getCache = async (key) => {
+    const data = await client.get(key);
+    return data ? JSON.parse(data) : null;  // Nếu có cache thì trả về dữ liệu JSON
+};
 
-// // Hàm set dữ liệu vào Redis Cache
-// exports.setCache = async (key, value, ttl = 60) => {
-//     await client.setEx(key, ttl, JSON.stringify(value));  // TTL: Thời gian sống (tính bằng giây)
-// };
+// Hàm set dữ liệu vào Redis Cache
+exports.setCache = async (key, value, ttl = 60) => {
+    await client.setEx(key, ttl, JSON.stringify(value));  // TTL: Thời gian sống (tính bằng giây)
+};
 
-// // Hàm xoá Cache (thường dùng khi dữ liệu thay đổi)
-// exports.clearCache = async (key) => {
-//     await client.del(key);
-// };
+// Hàm xoá Cache (thường dùng khi dữ liệu thay đổi)
+exports.clearCache = async (key) => {
+    await client.del(key);
+};
+
 
